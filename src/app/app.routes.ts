6472217e74db84d4fd9fb73cbf0f0b0537aa31f5
Alex-ander12 <wilsonalexandercusihuaman@gmail.com>
2024-12-05 10:31:08 -0500

    creacion de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+// app.routes.ts
+import { Route } from '@angular/router';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';  // Asegúrate de que esta ruta esté bien configurada
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { RedComponent } from './components/red/red.component';
 
-export const routes: Routes = [];
+export const routes: Route[] = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },  
+];
