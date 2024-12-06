b333296204ee54d39b0f849c278940f5dd88878e
Alex-ander12 <wilsonalexandercusihuaman@gmail.com>
2024-12-06 15:58:53 -0500

    creacion de post con database

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,72 @@
 import { Component } from '@angular/core';
-
+import { Auth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider, FacebookAuthProvider } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router'; 
+import { RouterLink, RouterLinkActive, RouterLinkWithHref } from '@angular/router';
+ 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkWithHref, RouterLinkActive],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  constructor(private auth: Auth, private router: Router) {} 
 
-}
+  login(email: string, password: string) {
+    signInWithEmailAndPassword(this.auth, email, password)
+      .then((userCredential) => {
+        console.log('Usuario logueado exitosamente:', userCredential.user);
+        
+        // Obtener el token y almacenarlo
+        userCredential.user.getIdToken().then((token) => {
+          localStorage.setItem('authToken', token);
+          console.log('Token guardado:', token);
+          
+          // Navegar a la página de inicio
+          this.router.navigate(['/home']);
+        });
+      })
+      .catch((error) => {
+        console.error('Error al iniciar sesión:', error.message);
+        alert("El usuario no existe");
+      });
+  }
+  
+  loginWithGoogle() {
+    const googleProvider = new GoogleAuthProvider();
+    signInWithPopup(this.auth, googleProvider)
+      .then((userCredential) => {
+        console.log('Usuario logueado con Google:', userCredential.user);
+  
+        userCredential.user.getIdToken().then((token) => {
+          localStorage.setItem('authToken', token);
+          console.log('Token guardado:', token);
+  
+          this.router.navigate(['/home']);
+        });
+      })
+      .catch((error) => {
+        console.error('Error al iniciar sesión con Google:', error.message);
+      });
+  }
+  
+  loginWithFacebook() {
+    const facebookProvider = new FacebookAuthProvider();
+    signInWithPopup(this.auth, facebookProvider)
+      .then((userCredential) => {
+        console.log('Usuario logueado con Facebook:', userCredential.user);
+  
+        userCredential.user.getIdToken().then((token) => {
+          localStorage.setItem('authToken', token);
+          console.log('Token guardado:', token);
+  
+          this.router.navigate(['/home']);
+        });
+      })
+      .catch((error) => {
+        console.error('Error al iniciar sesión con Facebook:', error.message);
+      });
+  }
+}  
--- src/app/components/red/red.component.ts
+++ src/app/components/red/red.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { Observable, from } from 'rxjs'; 
+import { Firestore, collection, getDocs, DocumentData } from '@angular/fire/firestore'; 
+import { map } from 'rxjs/operators';
+import { Router } from '@angular/router'; 
+import { RouterLink, RouterLinkActive, RouterLinkWithHref } from '@angular/router';
 @Component({
   selector: 'app-red',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink, RouterLinkWithHref, RouterLinkActive],  
   templateUrl: './red.component.html',
-  styleUrl: './red.component.css'
+  styleUrls: ['./red.component.css']
 })
 export class RedComponent {
+  posts$: Observable<any>;
 
+  constructor(private firestore: Firestore) {
+    this.posts$ = this.getPosts(); 
+  }
+
+  getPosts(): Observable<any> {
+    const postsCollection = collection(this.firestore, "posts");
+
+    return from(getDocs(postsCollection)).pipe(
+      map(querySnapshot => {
+       
+        return querySnapshot.docs.map(doc => {
+          const data = doc.data() as DocumentData;  
+          return {
+            imageBase64: data['imageBase64'],  
+            createdAt: data['createdAt']
+          };
+        });
+      })
+    ); 
+  }
 }
